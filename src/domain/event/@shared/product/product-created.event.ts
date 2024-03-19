4f8c04d6c4fe1eed8903a47a4503f75157265f8b
flavio <flavi@DESKTOP-USTCPSC>
2024-03-18 21:24:34 -0300

    feat: create event domain

---
+++ src/domain/event/@shared/product/product-created.event.ts
@@ -0,0 +1,13 @@
+import EventInterface from "../event.interface";
+
+export default class ProductCreatedEvent implements EventInterface {
+    dateTimeOccurred: Date;
+    eventData: any;
+
+
+    constructor(eventData:any) {
+        this.dateTimeOccurred = new Date();
+        this.eventData = eventData;
+    }
+    
+}
