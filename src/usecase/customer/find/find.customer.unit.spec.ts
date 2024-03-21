fb50be1f1f2a5143cdeba5562fc657da22e0c4be
flavio <flavi@DESKTOP-USTCPSC>
2024-03-21 18:02:30 -0300

    test: create customer unit and integration test

---
+++ src/usecase/customer/find/find.customer.unit.spec.ts
@@ -0,0 +1,67 @@
+import CustomerRepository from "../../../infrastructure/customer/repository/sequelize/customer.repository";
+import Customer from "../../../domain/customer/entity/customer";
+import Address from "../../../domain/customer/value-object/address";
+import FindCustomerUseCase from "./find.customer.usecase";
+
+
+const customer = new Customer("123", "John")
+const address = new Address("Street", 123, "Zip", "City")
+customer.changeAddress(address);
+
+const MockRepository = () => {
+    return {
+        find: jest.fn().mockReturnValue(Promise.resolve(customer)),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn()
+    }
+}
+
+describe("Unit Test find customer usecase", ()=>{
+  
+    it("should find a customer", async ()=>{
+        
+        const customerRepository = MockRepository();
+        const useCase = new FindCustomerUseCase(customerRepository);
+
+        const input = {
+            id: "123"
+        }
+
+        const output = {
+            id:"123",
+            name:"John",
+            address: {
+                street:"Street",
+                city:"City",
+                number:123,
+                zip:"Zip"
+            }
+        }
+
+        const result = await useCase.execute(input);
+
+        expect(result).toEqual(output);
+
+    })
+
+
+    it("should not find a customer", () => {
+        const customerRepository = MockRepository();
+
+        customerRepository.find.mockImplementation(() => {
+            throw new Error("Customer not found");
+        })
+
+        const useCase = new FindCustomerUseCase(customerRepository);
+
+        const input = {
+            id: "123"
+        }
+
+
+        expect(()=>{
+            return useCase.execute(input);
+        }).rejects.toThrow("Customer not found")
+    })
+})
