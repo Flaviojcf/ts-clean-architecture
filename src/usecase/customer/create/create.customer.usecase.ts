ffccfe4834e4c86e456f8ff165e84a92d6b5ccbe
flavio <flavi@DESKTOP-USTCPSC>
2024-03-21 23:02:03 -0300

    feat: Add create customer useCase

---
+++ src/usecase/customer/create/create.customer.usecase.ts
@@ -0,0 +1,38 @@
+import { v4 as uuid } from "uuid";
+import CustomerRepositoryInterface from "../../../domain/customer/repository/customer-repository-interface";
+import { InputCreateCustomerDto, OutputCreateCustomerDto } from "./create.customer.dto";
+import CustomerFactory from "../../../domain/customer/factory/customer.factory";
+import Address from "../../../domain/customer/value-object/address";
+
+export default class CreateCustomerUseCase {
+    private _customerRepository: CustomerRepositoryInterface;
+
+    constructor(customerRepository:CustomerRepositoryInterface) {
+        this._customerRepository = customerRepository
+    }
+
+    async execute(input: InputCreateCustomerDto) : Promise<OutputCreateCustomerDto> {
+        const customer = CustomerFactory.createWithAddress(
+            input.name, 
+            new Address(
+                input.address.street,
+                input.address.number,
+                input.address.zip,
+                input.address.city
+            ));
+
+        await this._customerRepository.create(customer);
+
+        return {
+            id:customer.id,
+            name:customer.name,
+            address: {
+                street:customer.Address.street,
+                city:customer.Address.city,
+                number:customer.Address.number,
+                zip:customer.Address.zip
+
+            }
+        }
+    }
+}
